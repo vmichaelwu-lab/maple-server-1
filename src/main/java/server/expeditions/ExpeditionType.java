eede8b988672fdcdfee707a2598ada2b804d9fe0
Michael Wu <michael@MacBookPro.attlocal.net>
2025-10-01 21:27:17 -0700

    changed min expedition size to 1 for all bosses

--- src/main/java/server/expeditions/ExpeditionType.java
+++ src/main/java/server/expeditions/ExpeditionType.java
@@ -29,19 +29,19 @@ import config.YamlConfig;
  */
 
 public enum ExpeditionType {
-    BALROG_EASY(3, 30, 50, 255, 5),
-    BALROG_NORMAL(6, 30, 50, 255, 5),
-    SCARGA(6, 30, 100, 255, 5),
-    SHOWA(3, 30, 100, 255, 5),
-    ZAKUM(6, 30, 50, 255, 5),
-    HORNTAIL(6, 30, 100, 255, 5),
-    CHAOS_ZAKUM(6, 30, 120, 255, 5),
-    CHAOS_HORNTAIL(6, 30, 120, 255, 5),
-    ARIANT(2, 7, 20, 30, 5),
-    ARIANT1(2, 7, 20, 30, 5),
-    ARIANT2(2, 7, 20, 30, 5),
-    PINKBEAN(6, 30, 120, 255, 5),
-    CWKPQ(6, 30, 90, 255, 5);   // CWKPQ min-level 90, found thanks to Cato
+    BALROG_EASY(1, 30, 50, 255, 5),
+    BALROG_NORMAL(1, 30, 50, 255, 5),
+    SCARGA(1, 30, 100, 255, 5),
+    SHOWA(1, 30, 100, 255, 5),
+    ZAKUM(1, 30, 50, 255, 5),
+    HORNTAIL(1, 30, 100, 255, 5),
+    CHAOS_ZAKUM(1, 30, 120, 255, 5),
+    CHAOS_HORNTAIL(1, 30, 120, 255, 5),
+    ARIANT(1, 7, 20, 30, 5),
+    ARIANT1(1, 7, 20, 30, 5),
+    ARIANT2(1, 7, 20, 30, 5),
+    PINKBEAN(1, 30, 120, 255, 5),
+    CWKPQ(1, 30, 90, 255, 5); // CWKPQ min-level 90, found thanks to Cato
 
     private final int minSize;
     private final int maxSize;
